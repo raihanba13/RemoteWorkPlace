a4dd5b334cae8406465ba65c406b37ecb44ce57e
raihanba13 <raihanba13@users.noreply.github.com>
2021-12-10 00:00:30 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,78 @@
+from fastapi import FastAPI, Depends 
+from pydantic import BaseModel 
+from typing import Optional, List 
+from sqlalchemy import create_engine 
+from sqlalchemy.orm import declarative_base, sessionmaker, Session 
+from sqlalchemy import Boolean, Column, Float, String, Integer 
+
+app = FastAPI()
+
+SQLALCHEMY_DATABASE_URL = 'sqlite+pysqlite:///./db.sqlite3'
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True, future=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db 
+    finally:
+        db.close()
+
+class DBPlace(Base):
+    __tablename__ = 'places'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(50))
+    description = Column(String, nullable=True)
+    coffee = Column(Boolean)
+    wifi = Column(Boolean)
+    food = Column(Boolean)
+    lat = Column(Float)
+    lng = Column(Float)
+
+Base.metadata.create_all(bind=engine)
+
+class Place(BaseModel):
+    name: str 
+    description: Optional[str] = None
+    coffee: bool 
+    wifi: bool 
+    food: bool 
+    lat: float 
+    lng: float 
+
+    class Config:
+        orm_mode = True 
+
+def get_place(db: Session, place_id: int):
+    return db.query(DBPlace).where(DBPlace.id == place_id).first()
+
+def get_places(db: Session):
+    return db.query(DBPlace).all()
+
+def create_place(db: Session, place: Place):
+    db_place = DBPlace(**place.dict())
+    db.add(db_place)
+    db.commit()
+    db.refresh(db_place)
+
+    return db_place
+
+# routes for interacting with the API
+@app.post('/places/', response_model=Place)
+def create_places_view(place: Place, db: Session = Depends(get_db)):
+    db_place = create_place(db, place)
+    return db_place
+
+@app.get('/places/', response_model=List[Place])
+def get_places_view(db: Session = Depends(get_db)):
+    return get_places(db)
+
+@app.get('/place/{place_id}')
+def get_place_view(place_id: int, db: Session = Depends(get_db)):
+    return get_place(db, place_id)
+
+@app.get('/')
+async def root():
+    return {'message': 'Hello world'}
